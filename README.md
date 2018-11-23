b61a6779e408db4e3c97650c4059ae62a4573c16
Nar Cuenca <narc.ph@gmail.com>
2018-11-23 13:51:05 +0800

    initial project setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# PDAX Order Book Trading Exam
+
+---
+
+### To use:
+
+---
+
+1. Install dependencies: `npm install`.
+2. Run server: `npm run start`.
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
